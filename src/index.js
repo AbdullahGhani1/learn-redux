22253f30637a94f809e4e349a926d1eebb51e798
AbdullahGhani1 <engr.abdullahghani@gmail.com>
2022-04-21 03:31:25 +0500

    subscribing to the store

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import store from "./store";
+const unsubscribe=store.subscribe(() => {
+    console.log("Store changed!",store.getState());
+})
 store.dispatch({ type: "bugAdded", payload: { description: "Bug 1" } });
+unsubscribe();
 store.dispatch({ type: "bugRemoved", payload: { id: 1 } });
 console.log(store.getState());
