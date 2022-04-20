67188666cbb3729a3a26ba713a8cf78874a4dc4a
AbdullahGhani1 <engr.abdullahghani@gmail.com>
2022-04-21 03:37:01 +0500

    action Types

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import store from "./store";
+import  * as actions from "./actionTypes";
+
+// state =reduced(state, action);
+// notify the Subscribers
+
+
 const unsubscribe=store.subscribe(() => {
     console.log("Store changed!",store.getState());
 })
-store.dispatch({ type: "bugAdded", payload: { description: "Bug 1" } });
+store.dispatch({ type: actions.BUG_ADDED, payload: { description: "Bug 1" } });
 unsubscribe();
-store.dispatch({ type: "bugRemoved", payload: { id: 1 } });
+store.dispatch({ type: actions.BUG_REMOVED, payload: { id: 1 } });
 console.log(store.getState());
--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,8 @@
+import  * as actions from "./actionTypes";
 let lastId = 0;
 export default function reducer(state = [], action) {
   switch (action.type) {
-    case "bugAdded":
+    case actions.BUG_ADDED:
       return [
         ...state,
         {
@@ -10,7 +11,7 @@ export default function reducer(state = [], action) {
           resolved: false,
         },
       ];
-    case "bugRemoved":
+    case actions.BUG_REMOVED:
       return state.filter((bug) => bug.id !== action.payload.id);
       default:
           return state;
