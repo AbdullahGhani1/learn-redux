a944e7a1a63fae50ebe267bd21811564b5c93662
AbdullahGhani1 <engr.abdullahghani@gmail.com>
2022-04-21 04:15:12 +0500

    action Creater

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import store from "./store";
-import  * as actions from "./actionTypes";
-
+import {bugAdded,bugRemoved} from "./actions";
 // state =reduced(state, action);
 // notify the Subscribers
 
 
 const unsubscribe=store.subscribe(() => {
     console.log("Store changed!",store.getState());
-})
-store.dispatch({ type: actions.BUG_ADDED, payload: { description: "Bug 1" } });
+})  
+store.dispatch(bugAdded("Bug 1"));
 unsubscribe();
-store.dispatch({ type: actions.BUG_REMOVED, payload: { id: 1 } });
+store.dispatch(bugRemoved(1));
 console.log(store.getState());
